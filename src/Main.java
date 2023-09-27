5a0bc2b945a9c3eb5151e01477180c5d70220fb7
613668 <beres.613668@swl.k12.oh.us>
2023-09-27 09:42:00 -0400

    fgdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        int birthMonth;
+
+        System.out.println("Enter your birth month: ");
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is " + birthMonth);
+            } else {
+                System.out.println("INVALID!!!!!!!!!!");
+            }
+        } else {
+            System.out.println("INVALID!!!!!!!!");
+        }
     }
 }
